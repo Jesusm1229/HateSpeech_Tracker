176208aecf0e8157807267ec44a8eb7fac9c2593
Unusual-Waffles-Situation <62034860+Unusual-Waffles-Situation@users.noreply.github.com>
2021-05-29 22:44:53 +0000

    Add files via upload
    Main module for the data-fetching functions.

---
+++ TweetScrappingFunctions.py
@@ -0,0 +1,8 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as import pd
+
+# Variables
+maxTweets = 100    # Max amount of tweets to be saved
+
+tweets_list = []    # Saves the tweets in a list
+
