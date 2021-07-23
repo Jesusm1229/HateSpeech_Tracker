8eeb895128f5baad4ed60f41c6e1dcccfb245c81
Unusual-Waffles-Situation <62034860+Unusual-Waffles-Situation@users.noreply.github.com>
2021-07-23 13:40:20 +0000

    Update TweetScrappingFunctions.py
    Final changes to this.

--- TweetScrappingFunctions.py
+++ TweetScrappingFunctions.py
@@ -3,7 +3,6 @@ import pandas as pd
 from datetime import date
 
 # Variables
-maxTweets = 50    # Max amount of tweets to be saved
 
 tweets_list = []    # Saves the tweets in a list
 
@@ -20,13 +19,17 @@ def tweets_fetching():
 
     count = 0
 
+    idCounter = 1    # Variable for the id counter
+
+    maxTweets = 500   # Max amount of tweets to be saved
+
     currentDate = today.strftime("%Y-%m-%d")
 
     while count < listLength:
 
         word = wordList[count]
 
-        stringForFunction = word + ' since:2021-03-01 until:' + currentDate
+        stringForFunction = word + ' since:2021-01-01 until:' + currentDate    # This searches for tweets between the first day of the year and the current day
 
         i = 0
 
@@ -34,12 +37,17 @@ def tweets_fetching():
             
             content = tweet[1].content
 
-            content = content.lower()    # This lower-case the content of the Tweet
+            contentLowerCased = content.lower()    # This lower-case the content of the Tweet
 
-            if word in content:    # Condition to check if the word is contained in the content of the Tweet
-                tweets_list.append([tweet[1].content, tweet[1].user.location, tweet[1].user.username])
+            if word in contentLowerCased:    # Condition to check if the word is contained in the content of the Tweet
+
+                content = clean_tweets(content)
+
+                tweets_list.append([idCounter, content, tweet[1].user.location, tweet[1].user.username])
 
                 i += 1
+
+                idCounter += 1
 
             if i > maxTweets:
                 break
@@ -51,14 +59,14 @@ def tweets_fetching():
 def create_dataframe(tweets_list):
     '''Function to create a dataframe using the list'''
 
-    tweets_df = pd.DataFrame(tweets_list, columns=['Text', 'Location', 'Username'])
+    tweets_df = pd.DataFrame(tweets_list, columns=['Id', 'Text', 'Location', 'Username'])
 
     return tweets_df
 
 def create_csv_file(tweets_df):
     '''Function to create a CSV file using the previously created dataframe'''
 
-    tweets_df.to_csv('user-tweets.csv', sep=',', index=False)
+    tweets_df.to_csv('user-tweets.csv', sep = ',', index = False)
 
 def open_file():
     '''Function for reading the file containing the words to be searched'''
@@ -91,3 +99,15 @@ def clean_list(wordList):
             cleanList.append(word)
 
     return cleanList
+
+def clean_tweets(tweetContent):
+    '''Function used to clean the tweets containing the new line character. So all the tweet content is stored in a single line, and not, potentially, multiples'''
+
+    newLineCheck = "\n"
+
+    if newLineCheck in tweetContent:    # Check if the new line character is in the tweet
+        cleanedTweet = tweetContent.replace("\n", " ")
+
+        return cleanedTweet
+
+    return tweetContent
